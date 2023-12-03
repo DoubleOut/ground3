672330b6daf3fff4769fab4de7716e6211ef1355
Ian <3082644+DoubleOut@users.noreply.github.com>
2023-12-03 18:59:16 -0500

    wip

--- main.ts
+++ main.ts
@@ -1,15 +1,28 @@
 radio.onReceivedValue(function (name, value) {
-    serial.writeValue(name, value)
-    if (iRxCount > 5) {
-        iRxCount = 0
-        serial.writeValue("distance", radio.receivedPacket(RadioPacketProperty.SignalStrength) + 128)
-    } else {
-        iRxCount += 1
+    if (name == "pitch") {
+        pitch = value
     }
+    if (name == "roll") {
+        roll = value
+    }
+    if (name == "heading") {
+        heading = value
+    }
+    if (name == "uTi") {
+        uTi = value
+    }
+    strength = radio.receivedPacket(RadioPacketProperty.SignalStrength)
 })
-let iRxCount = 0
+let strength = 0
+let uTi = 0
+let heading = 0
+let roll = 0
+let pitch = 0
 radio.setGroup(43)
-iRxCount = 0
+loops.everyInterval(1000, function () {
+    serial.writeValue("heading", heading)
+    serial.writeNumbers([uTi, Math.abs(strength)])
+})
 basic.forever(function () {
 	
 })
