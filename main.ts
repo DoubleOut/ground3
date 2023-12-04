fab7fa7d3ffd93b692bfea807589c7eca05f8c8f
Ian <3082644+DoubleOut@users.noreply.github.com>
2023-12-03 19:07:52 -0500

    wip

--- main.ts
+++ main.ts
@@ -22,6 +22,11 @@ radio.setGroup(43)
 loops.everyInterval(1000, function () {
     serial.writeValue("heading", heading)
     serial.writeNumbers([uTi, Math.abs(strength)])
+    if (randint(0, 2) > 0) {
+        led.plot(randint(0, 4), randint(2, 4))
+    } else {
+        led.unplot(randint(0, 4), randint(2, 4))
+    }
 })
 basic.forever(function () {
 	
